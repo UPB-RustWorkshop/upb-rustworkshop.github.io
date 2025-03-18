08b7f24a25a8a5a8412a11802186029f65ba34f1
Alexandru RADOVICI <alexandru.radovici@wyliodrin.com>
2025-03-18 18:46:59 +0200

    rust workshop website

---
+++ docs/intro.md
@@ -0,0 +1,55 @@
+---
+sidebar_position: 1
+---
+# Welcome
+
+Welcome to the Rust Workshop :crab:
+
+## What to bring
+
+Please make sure you have:
+- a laptop with Rust working (we prefer Linux, macOS or a Linux VM)
+- a GitHub account
+- read chapters [2](https://doc.rust-lang.org/book/ch02-00-guessing-game-tutorial.html), [3](https://doc.rust-lang.org/book/ch03-00-common-programming-concepts.html) and [5](https://doc.rust-lang.org/book/ch05-00-structs.html) from *The Rust Programming Language*
+- do the [Tour of Rust](https://tourofrust.com)
+
+## Schedule
+
+Please take a moment to look at the schedule for the Rust Workshop.
+
+### Saturday
+
+| Time | Location | Activity |
+|------|----------|----------|
+|10:30 - 11:30 | PR001 | What makes Rust so special? - Alexandru Radovici |
+|11:30 - 13:00 | Breakout Rooms | Workshop (for each track) |
+|13:00 - 14:00 | Lunch | Pizza from NXP |
+|14:00 - 18:00 | Breakout Rooms | Workshop (for each track) |
+
+### Sunday
+
+| Time | Location | Activity |
+|------|----------|----------|
+|10:00 - 13:00 | Breakout Rooms | Open Source Hackathon |
+|13:00 - 14:00 | Lunch | Pizza from NXP |
+|14:00 - 18:00 | Breakout Rooms | Open Source Hackathon |
+
+## Tracks
+
+Track assignments will be announced when registering.
+
+| Track | Room | Trainers |
+|-------|------|----------|
+| Embassy | | |
+| Web Servers | | |
+| Slint Desktop Applications | |  |
+| Tauri Desktop Applications | |  |
+| Command Line Applications | PR001 | |
+
+## Thank you
+
+This event would not have been possible without the help of:
+- The [Computer Engineering Department](https://cs.pub.ro/), Politehnica University of Bucharest
+- The [PRECIS Research Center](http://precis.acs.pub.ro/), Politehnica University of Bucharest
+- [Wyliodrin](https://wyliodrin.com)
+- [NXP Semiconductors Romania](https://www.nxp.com/company/about-nxp/worldwide-locations/romania:ROMANIA_HOME_ROMANIAN)
---
+++ src/components/HomepageFeatures/index.tsx
@@ -0,0 +1,72 @@
+import type { ReactNode } from "react";
+import clsx from "clsx";
+import Heading from "@theme/Heading";
+import styles from "./styles.module.css";
+
+type FeatureItem = {
+  title: string;
+  Svg: React.ComponentType<React.ComponentProps<"svg">>;
+  description: ReactNode;
+};
+
+const FeatureList: FeatureItem[] = [
+  {
+    title: "Web Servers",
+    Svg: require("@site/static/img/html5.svg").default,
+    description: (
+      <>
+        Learn how to leverage Rust's typesafety to build efficient web servers.
+      </>
+    ),
+  },
+  {
+    title: "Desktop Applications",
+    Svg: require("@site/static/img/desktop.svg").default,
+    description: (
+      <>
+        Learn how to use <a href="https://tauri.app">Tauri</a> and web
+        technologies to build modern desktop apps or{" "}
+        <a href="https://slint.dev">Slint</a> to build native desktop apps in
+        Rust.
+      </>
+    ),
+  },
+  {
+    title: "Embedded",
+    Svg: require("@site/static/img/embedded.svg").default,
+    description: (
+      <>
+        Use the Raspberry Pi Pico 2W, one of the most affordable and easy to use
+        development boards to design and implement hardware devices.
+      </>
+    ),
+  },
+];
+
+function Feature({ title, Svg, description }: FeatureItem) {
+  return (
+    <div className={clsx("col col--4")}>
+      <div className="text--center">
+        <Svg className={styles.featureSvg} role="img" />
+      </div>
+      <div className="text--center padding-horiz--md">
+        <Heading as="h3">{title}</Heading>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default function HomepageFeatures(): ReactNode {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
