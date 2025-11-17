fba8b1e2633548ccbf99f436d11119b2f83cc50d
Alexandru Radovici <alexandru.radovici@wyliodrin.com>
2025-11-17 18:48:07 +0200

    kernel: add arm64 (#28)
    * add arm support
    
    * add arm64 dev
    
    * fix typo
    
    Co-authored-by: Irina Nita <irina.nita@posteo.com>
    
    * fix typo
    
    Co-authored-by: Irina Nita <irina.nita@posteo.com>
    
    ---------
    
    Co-authored-by: Irina Nita <irina.nita@posteo.com>

---
+++ docs/linux_kernel_development/3.poweroff/index.md
@@ -0,0 +1,3 @@
+# Power Off Module
+
+Powering off QEMU is done differently for [x86_64](poweroff_x86) and for [ARM64](poweroff_arm64). Please select one of the platform.
--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -139,7 +139,7 @@ const config: Config = {
     prism: {
       theme: prismThemes.github,
       darkTheme: prismThemes.dracula,
-      additionalLanguages: ["bash", "toml", "makefile"],
+      additionalLanguages: ["bash", "toml", "makefile", "diff"],
     },
   } satisfies Preset.ThemeConfig,
 };
